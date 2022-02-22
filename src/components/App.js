e8dd2b39408cd2782e48eabad03ec65ddcce7e6c
Charlie Davis-Miller <charlie.davismiller@gmail.com>
2022-02-22 17:49:56 +0000

    added Input component

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React from 'react'
 
 import {Header} from './Header'
+import { Input } from './Input'
 
-import './App.css';
+import './App.css'
 
 export const App = () => {
   return (
     <div className="App">
       <Header />
+      <Input />
     </div>
   );
 }
---
+++ src/components/Input.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export const Input = () => {
+  return (
+    <form className="InputForm">
+      <input></input>
+      <button type='submit'>Add Todo</button>
+    </form>
+  )
+}
