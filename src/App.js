257abc1430f3c7be6b12ec1c5008e294e24467d2
Charlie Davis-Miller <charlie.davismiller@gmail.com>
2022-02-22 12:46:42 +0000

    created Header component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
+
+import {Header} from './Header'
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Redux Todo App</h1>
-      <img src='/logo192.png' alt='redux logo' className='ReduxLogo' />
-      <h3>A place to enter your todos into a high quality state management system</h3>
+      <Header />
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Header = () => {
+  return (
+    <div>
+      <h1>Redux Todo App</h1>
+      <img src='/logo192.png' alt='redux logo' className='ReduxLogo' />
+      <h3>A place to enter your todos into a high quality state management system</h3>
+    </div>
+  )
+}
