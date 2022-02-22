5cd43c859b88efe140fc62fc74d889aaa5c13d15
Charlie Davis-Miller <charlie.davismiller@gmail.com>
2022-02-22 12:58:46 +0000

    removed h1

--- src/Header.js
+++ src/Header.js
@@ -3,9 +3,8 @@ import React from 'react'
 export const Header = () => {
   return (
     <div>
-      <h1>Redux Todo App</h1>
       <img src='/logo192.png' alt='redux logo' className='ReduxLogo' />
-      <h3>A place to enter your todos into a high quality state management system</h3>
+      <h3>a place to enter todos into a high quality state management system</h3>
     </div>
   )
 }
