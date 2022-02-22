fcfe32926709a713d41fae0c2ce494e59b56fa34
Charlie Davis-Miller <charlie.davismiller@gmail.com>
2022-02-22 12:41:04 +0000

    header, subheader and redux logo added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <h1>Redux Todo App</h1>
+      <img src='/logo192.png' alt='redux logo' className='ReduxLogo' />
+      <h3>A place to enter your todos into a high quality state management system</h3>
     </div>
   );
 }
