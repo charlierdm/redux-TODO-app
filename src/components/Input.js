16c6569e34cc943ba99c475b3c45074f66480a94
Charlie Davis-Miller <charlie.davismiller@gmail.com>
2022-02-22 17:54:14 +0000

    added form to Input component

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,10 +1,18 @@
-import React from "react"
+import React, {useState} from "react"
 
 export const Input = () => {
+  const [todo, setTodo] = useState('')
+
   return (
     <form className="InputForm">
-      <input></input>
-      <button type='submit'>Add Todo</button>
+      <input 
+        type='text' 
+        placeholder="what do you need to do?"
+        onChange={(e) => setTodo(e.target.value)}
+        value={todo} />
+      <button type='submit'>
+        Add Todo
+      </button>
     </form>
   )
 }
