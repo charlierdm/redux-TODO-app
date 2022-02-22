2418cd8f1ce428021cfeacb14fd5c9fde313c7aa
Charlie Davis-Miller <charlie.davismiller@gmail.com>
2022-02-22 18:28:17 +0000

    Input form styling

--- src/components/Input.js
+++ src/components/Input.js
@@ -3,19 +3,22 @@ import React, {useState} from "react"
 export const Input = () => {
   const [todo, setTodo] = useState('')
 
-  const addTodo = () => {
+  const addTodo = (e) => {
+    e.preventDefault()
     setTodo('')
   }
 
   return (
-    <div className="Input">
+    <form className="Input">
       <input 
         type='text' 
         onChange={(e) => setTodo(e.target.value)}
-        value={todo} />
-      <button type='submit' onClick={addTodo}>
+        value={todo} 
+        placeholder='enter a new todo...'
+      />
+      <button type='submit' onClick={(e) => addTodo(e)}>
         Add Todo
       </button>
-    </div>
+    </form>
   )
 }
