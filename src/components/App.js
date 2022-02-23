beb17417a943ee9598a3123e8142700168d54ec0
Charlie Davis-Miller <charlie.davismiller@gmail.com>
2022-02-23 10:03:48 +0000

    reverted redux settings to run through steps again

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import {Header} from './Header'
-import { Input } from './Input'
+import {Input} from './Input'
 
 import './App.css'
 
--- src/components/Input.js
+++ src/components/Input.js
@@ -1,46 +1,24 @@
 import React, {useState} from "react"
-import {connect, useDispatch} from 'react-redux'
-
-import {addTodo} from "../redux/todoSlice"
-
-const mapStateToProps = (state) => {
-  return {
-    todos: state,
-  }
-}
-
-const mapDispatchToProps = (dispatch) => {
-  return {
-    addTodo: (obj) => dispatch(addTodo(obj))
-  }
-}
 
 export const Input = () => {
   const [todo, setTodo] = useState('')
-  const dispatch = useDispatch()
 
-  const addTodo = () => {
-    dispatch(addTodo({
-      item: todo,
-      complete: false,
-      id: Math.floor(Math.random() * 1000),
-    }))
+  const addTodo = e => {
+    e.preventDefault()
     setTodo('')
   }
 
   return (
-    <div className="Input">
+    <form className="Input">
       <input 
         type='text' 
         onChange={e => setTodo(e.target.value)}
         value={todo} 
         placeholder='enter a new todo...'
       />
-      <button onClick={() => addTodo()}>
+      <button onClick={e => addTodo(e)}>
         Add Todo
       </button>
-    </div>
+    </form>
   )
 }
-
-export default connect(mapStateToProps, mapDispatchToProps)(Input);
